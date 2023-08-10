84ea081f88bd5f781e81ae59e0ac5e3bcd41b232
gabrielvaimberg04 <gavaimberg@gmail.com>
2023-08-09 23:19:40 -0300

    react

---
+++ mkdir src/components/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Contenido de la aplicación</h1>
+    </div>
+  );
+}
+
+
+
+export default App;
